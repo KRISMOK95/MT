43568ad9e4eb4cebb92cf360de37c65850d840aa
KRISMOK95 <71318548+KRISMOK95@users.noreply.github.com>
2023-02-17 10:51:30 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -11,12 +11,26 @@ print(response_json)
 
 ####
 # POST
+num1 = 5
+num2 = 7
+total = num1 + num2
+
+new_num ={
+    "total": total
+}
+
+
 new_data = {
     "id": 4, "title": "explosion"
 }
+
+
 url_post = "https://my-json-server.typicode.com/KRISMOK95/MT/Failures/"
 
 post_response = requests.post(url_post, json=new_data)
+post_response0 = requests.post(url_post, json=new_num)
 
 post_response_json = post_response.json()
+post_response_json0 = post_response0.json()
 print(post_response_json)
+print(post_response_json0)
